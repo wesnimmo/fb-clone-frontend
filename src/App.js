067a208254d09d1f9b94b39c5cd53c75295efd7b
Wes <wesnimmo@gmail.com>
2023-01-09 06:21:54 -0800

    first project commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  const get = async () => {
+    const res = await fetch("http://localhost:8000/user");
+    console.log(res);
+  };
+
+  get();
+  return <div>welcome to frontend</div>;
+}
+
+export default App;
